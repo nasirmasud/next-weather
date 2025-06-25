af702945a646d79170e4cc030710ab080190a1f1
Nasir Masud <nasir.masud@ymail.com>
2025-06-25 14:04:07 +0600

    Integrate getIconByDayNight utility for dynamic weather icon rendering based on time of day

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import Container from "@/components/Container";
 import Navbar from "@/components/Navbar";
+import getIconByDayNight from "@/utils/getIconByDayNight";
 import { kelvinToCelsius } from "@/utils/kelvinToCelsius";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
@@ -101,31 +102,60 @@ export default function Home() {
     <div className='flex flex-col gap-4 bg-gray-100 min-h-screen'>
       <Navbar />
       <main className='px-3 mx-auto flex flex-col gap-9 w-full pb-10 pt-4 max-w-7xl'>
-        <section>
-          <h2 className='flex gap-1 text-2xl items-end'>
-            <p>{format(parseISO(firstData?.dt_txt ?? ""), "EEEE")}</p>
-            <p className='text-lg'>
-              ({format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")})
-            </p>
-          </h2>
-          <Container className='gap-10 px-6 items-center'>
-            <div className='flex flex-col px-4'>
-              <span className='text-5xl'>
-                {kelvinToCelsius(firstData?.main.temp ?? 296.37)}°
-              </span>
-              <p className='text-s space-x-1 whitespace-nowrap'>
-                <span>Feels Like</span>
-                <span>{kelvinToCelsius(firstData?.main.feels_like ?? 0)}°</span>
+        <section className='space-y-3'>
+          <div className='space-y-2'>
+            <h2 className='flex gap-1 text-2xl items-end'>
+              <p>{format(parseISO(firstData?.dt_txt ?? ""), "EEEE")}</p>
+              <p className='text-lg'>
+                ({format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")})
               </p>
-            </div>
-          </Container>
+            </h2>
+            <Container className='gap-10 px-6 items-center'>
+              <div className='flex flex-col px-4'>
+                <span className='text-5xl'>
+                  {kelvinToCelsius(firstData?.main.temp ?? 296.37)}°
+                </span>
+                <p className='text-s space-x-1 whitespace-nowrap'>
+                  <span>Feels Like</span>
+                  <span>
+                    {kelvinToCelsius(firstData?.main.feels_like ?? 0)}°
+                  </span>
+                </p>
+                <p className='text-xs space-x-2'>
+                  <span>
+                    {kelvinToCelsius(firstData?.main.temp_min ?? 0)}°↓{" "}
+                  </span>
+                  <span>
+                    {kelvinToCelsius(firstData?.main.temp_max ?? 0)}°↑{" "}
+                  </span>
+                </p>
+              </div>
+              <div className='flex gap-10 sm:gap-16 overflow-x-auto w-full justify-between pr-3'>
+                {isClient &&
+                  data?.list.map((data, index) => (
+                    <div
+                      key={index}
+                      className='flex flex-col justify-between gap-2 items-center text-xs font-semibold'
+                    >
+                      <p className='whitespace-nowrap'>
+                        {format(parseISO(data.dt_txt), "h:mm a")}
+                      </p>
+                      <WeatherIcons
+                        iconName={getIconByDayNight(
+                          data.weather[0].icon,
+                          data.dt_txt
+                        )}
+                      />
+                      <p>{kelvinToCelsius(data.main.temp ?? 0)}°</p>
+                    </div>
+                  ))}
+              </div>
+            </Container>
+          </div>
+          <div className='flex gap-4'></div>
         </section>
-        <section className='flex gap-10 sm:gap-16 overflow-x-hidden w-full justify-between pr-3'>
-          {isClient &&
-            data?.list.map((item, index) => (
-              <WeatherIcons key={index} iconName={item.weather[0].icon} />
-            ))}
-        </section>
+
+        <section className='flex'></section>
       </main>
     </div>
   );
