c56cafcc01cfb83a81b753bbabd1dda73fc588b1
Nasir Masud <nasir.masud@ymail.com>
2025-07-05 17:42:21 +0600

    Refactor ForecastInDetail component to set default values for temperature props and improve layout structure

--- components/ForecastInDetail.tsx
+++ components/ForecastInDetail.tsx
@@ -19,30 +19,32 @@ export default function ForecastInDetail(props: ForecastInDetailProps) {
     weatherIcon = "10n",
     date = "19.09",
     day = "Tuesday",
-    temp,
-    feelsLike,
-    minTemp,
-    maxTemp,
-    description,
+    temp = 300.15,
+    feelsLike = 300.15,
+    minTemp = 300.15,
+    maxTemp = 300.15,
+    description = "Clear sky",
   } = props;
   return (
     <Container className='gap-4'>
       {/* Left Section */}
       <section className='flex gap-4 items-center px-4'>
-        <div>
+        <div className='flex flex-col items-center'>
           <WeatherIcons iconName={weatherIcon || "01d"} />
-          <p>{date}</p>
-          <p className='text-sm'>{day}</p>
-        </div>{" "}
+          <p>{date || "19.09"}</p>
+          <p className='text-sm'>{day || "Tuesday"}</p>
+        </div>
+        {/* Temperature */}
         <div className='flex flex-col px-4'>
           <span className='text-5xl'>{kelvinToCelsius(temp ?? 0)}°</span>
-          <p className='text-sm space-x-1 whitespace-nowrap'>
-            <span>Feels Like</span>{" "}
+          <p className='text-xs space-x-1 whitespace-nowrap'>
+            <span>Feels Like</span>
             <span>{kelvinToCelsius(feelsLike ?? 0)}°</span>
           </p>
           <p className='capitalize'>{description}</p>
         </div>
       </section>
+
       {/* Right Section */}
       <section className='flex justify-between w-full gap-4 px-4 pr-10 overflow-x-auto'>
         <WeatherDetails {...props} />
